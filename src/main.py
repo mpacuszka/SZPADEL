c8d7f198f073fefe9577ea92a0bf875263bb252d
Mateusz Pacuszka <mpacuszka@gmail.com>
2023-08-07 00:01:32 +0200

    Add initial code and tests

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
+from libs.my_lib import *
 import atexit
 
 def main():
-    print("Starting the app")
+    print(f"2**5 = {power_to('2', 5)}")
+    print(f"\"capitalize\" is now \"{capitalize('capitalize')}d\"")
 
 def exit_handler():
     print("Closing the app")
 
 if __name__ == "__main__":
+    print("Starting the app")
     main()
 
 atexit.register(exit_handler)
---
+++ tests/test_libs/test_my_lib.py
@@ -0,0 +1,22 @@
+from src.libs.my_lib import *
+
+def test_power_to_0_pow_any():
+    assert power_to(0, 2) == 0
+
+def test_power_to_any_pow_0():
+    assert power_to(2, 0) == 1
+
+def test_power_to_2_to_m2():
+    assert power_to(2, -2) == 0.25
+
+def test_power_to_none():
+    assert power_to("2", -2) == None
+
+def test_capitalize_ok():
+    assert capitalize("test") == "Test"
+
+def test_capitalize_numeric():
+    assert capitalize("123test") == "123test"
+
+def test_capitalize_none():
+    assert capitalize(3) == None
